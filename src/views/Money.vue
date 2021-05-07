d7a1c0cf46b1e57c391172e361e8c96bb3a798b5
ry <renyi2009@qq.com>
2021-05-07 14:05:13 +0800

    页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>Money
+  <div class="nav-wrapper">
+    <div class="content">Money</div>
     <Nav></Nav>
   </div>
 
@@ -14,5 +15,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
