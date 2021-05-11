5442cfc0e01f52a2434533a57bf944cf074b4c3e
ry <renyi2009@qq.com>
2021-05-11 18:40:44 +0800

    模块化的 store 删除了 models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,39 +1,13 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
   // record
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => {
-    recordListModel.create(record);
-  },
-
+  ...recordStore,
   // tag
-  tagList: tagListModel.fetch(),
-  findTag(id: string) {
-    return this.tagList.filter(t => t.id === id)[0];
-  },
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      alert('已经有这个标签名了');
-    } else if (message === 'success') {
-      alert('添加成功');
-    }
-  },
-
-  removeTag: (id: string) => {
-    /*if (tagListModel.remove(id)) {
-      return true;
-    } else {
-      return false;
-    }*/
-    return tagListModel.remove(id);
-  },
-
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  }
+  ...tagStore
 };
+
+console.log(store);
 
 export default store;
